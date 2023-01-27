f44bee5728b6d01472844055a51cd01b3e2bca5b
Kesse Jones <kjonesfc@outlook.com>
2023-01-26 23:19:15 -0300

    chore: remove unused configs

--- home-manager/nixos.nix
+++ home-manager/nixos.nix
@@ -67,8 +67,6 @@ in {
 
   programs.go.enable = true;
 
-  programs.nix-index.enable = true;
-
   home.packages = with pkgs; [
     neovim
     btop
@@ -109,37 +107,17 @@ in {
   xdg.configFile.fish.recursive = true;
   xdg.configFile.fish.source = "${dotfiles}/.config/fish";
 
-  xdg.configFile."gtk-2.0".recursive = true;
-  xdg.configFile."gtk-2.0".source = "${dotfiles}/.config/gtk-2.0";
-
-  xdg.configFile."gtk-3.0".recursive = true;
-  xdg.configFile."gtk-3.0".source = "${dotfiles}/.config/gtk-3.0";
-
-  xdg.configFile.btop.recursive = true;
-  xdg.configFile.btop.source = "${dotfiles}/.config/btop";
-
-  xdg.configFile.gh.recursive = true;
-  xdg.configFile.gh.source = "${dotfiles}/.config/gh";
-
-  xdg.configFile.rofi.recursive = true;
-  xdg.configFile.rofi.source = "${dotfiles}/.config/rofi";
-
-  xdg.configFile.mpv.recursive = true;
-  xdg.configFile.mpv.source = "${dotfiles}/.config/mpv";
-
-  xdg.dataFile.fonts.recursive = true;
-  xdg.dataFile.fonts.source = "${dotfiles}/.fonts";
-
   xdg.configFile.alacritty.recursive = true;
   xdg.configFile.alacritty.source = "${dotfiles}/.config/alacritty";
 
-  home.file.".gitconfig".source = "${dotfiles}/.gitconfig";
+  gtk = {
+    enable = true;
 
-  home.file.".icons".recursive = true;
-  home.file.".icons".source = "${dotfiles}/.icons";
-
-  home.file.".themes".recursive = true;
-  home.file.".themes".source = "${dotfiles}/.themes";
-
-  gtk.enable = true;
+    theme = {
+      name = "Catppuccin-Mocha-Standard-Blue-Dark";
+      package = pkgs.catppuccin-gtk.override {
+        variant = "mocha";
+      };
+    };
+  };
 }
