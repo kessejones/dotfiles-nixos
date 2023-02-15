bfa2d62d7149def353ddb1ff67887e9605bf913e
Kesse Jones <kjonesfc@outlook.com>
2023-02-14 22:43:22 -0300

    chore: remove unused config

--- system/hardware.nix
+++ system/hardware.nix
@@ -20,7 +20,6 @@
   swapDevices = [];
 
   networking.useDHCP = lib.mkDefault true;
-  networking.interfaces.enp3s0f1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
