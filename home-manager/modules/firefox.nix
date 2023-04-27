ba370abe454ec01c8468661689f7266bcec5181c
Kesse Jones <kjonesfc@outlook.com>
2023-04-26 21:17:07 -0300

    feat: added firefox custom settings

--- home-manager/modules/firefox.nix
+++ home-manager/modules/firefox.nix
@@ -3,55 +3,81 @@
     enable = true;
     profiles.default = {
       isDefault = true;
-      search.default = "DuckDuckGo";
       settings = {
-        "browser.ctrlTab.recentlyUsedOrder" = false;
-        "browser.search.hiddenOneOffs" = "";
-        "browser.urlbar.suggest.searches" = false;
+        # disable experiments
         "experiments.activeExperiment" = false;
         "experiments.enabled" = false;
         "experiments.supported" = false;
         "extensions.pocket.enabled" = false;
         "middlemouse.paste" = false;
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        "devtools.theme" = "dark";
+        "browser.zoom.siteSpecific" = false;
 
-        # Hardware acceleration related settings.
+        "browser.urlbar.suggest.quicksuggest.sponsored" = false;
+        "browser.urlbar.suggest.quicksuggest.nonsponsored" = false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+        "browser.newtabpage.activity-stream.showSponsored" = false;
+
+        "browser.newtabpage.activity-stream.telemetry" = false;
+        "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+
+        # privacy
+        "privacy.donottrackheader.enabled" = true;
+        "privacy.donottrackheader.value" = 1;
+        "privacy.firstparty.isolate" = true;
+        "privacy.trackingprotection.enabled" = true;
+        "privacy.trackingprotection.socialtracking.enabled" = true;
+        "privacy.trackingprotection.cryptomining.enabled" = true;
+        "privacy.trackingprotection.figerprinting.enabled" = true;
+        "toolkit.telemetry.enabled" = false;
+        "toolkit.telemetry.unified" = false;
+        "toolkit.telemetry.archive.enabled" = false;
+        "geo.enabled" = false;
+        "dom.battery.enabled" = false;
+        "network.IDN_show_punycode" = true;
+        "plugins.enumerable_names" = "";
+        "beacon.enabled" = false;
+        "browser.send_pings" = false;
+        "dom.webnotifications.enabled" = false;
+
+        "browser.ctrlTab.recentlyUsedOrder" = false;
+        "browser.search.hiddenOneOffs" = "";
+        "browser.urlbar.suggest.searches" = false;
+        "browser.toolbars.bookmarks.visibility" = "never";
+
+        # search engine
+        "browser.urlbar.placeholderName" = "DuckDuckGo";
+        "browser.search.defaultEngine" = "DuckDuckGo";
+
+        # homepage
+        "browser.selfsupport.url" = "";
+        "browser.aboutHomeSnippets.updateUrl" = "";
+        "browser.startup.homepage_override.mstone" = "ignore";
+        "browser.startup.homepage_override.buildId" = "";
+        "browser.homepage_welcome_url" = "";
+        "browser.homepage_welcome_url.additional" = "";
+        "browser.homepage_override_url" = "";
+
+        # hardware acceleration
         "gfx.webrender.all" = true;
-        "media.ffmpeg.vaapi.enabled" = true;
         "media.ffmpeg.vaapi-drm-display.enabled" = true;
+        "media.ffmpeg.vaapi.enabled" = true;
+        "media.ffvpx.enabled" = false;
+        "media.rdd-vpx.enabled" = false;
+        "gfx.webrender.compositor.force-enabled" = true;
+        "media.navigator.mediadatadecoder_vpx_enabled" = true;
+        "webgl.force-enabled" = true;
+        "layers.acceleration.force-enabled" = true;
+        "layers.offmainthreadcomposition.enabled" = true;
+        "layers.offmainthreadcomposition.async-animations" = true;
+        "layers.async-video.enabled" = true;
+        "html5.offmainthread" = true;
       };
     };
 
     package = pkgs.wrapFirefox pkgs.firefox-esr-unwrapped {
       extraNativeMessagingHosts = with pkgs.nur.repos.wolfangaukang; [vdhcoapp];
-      extraPolicies = {
-        CaptivePortal = false;
-        DisableFirefoxStudies = true;
-        DisablePocket = true;
-        DisableTelemetry = true;
-        DisableFirefoxAccounts = false;
-        FirefoxHome = {
-          Pocket = false;
-          Snippets = false;
-        };
-        UserMessaging = {
-          ExtensionRecomendations = false;
-          SkipOnboarding = true;
-        };
-      };
-
-      extraPrefs = ''
-        lockPref("security.identityblock.show_extended_validation", true);
-        lockPref("devtools.theme", "dark");
-        lockPref("media.peerconnection.enabled", false);
-        lockPref("geo.enabled", false);
-        lockPref("dom.battery.enabled", false);
-        lockPref("beacon.enabled", false);
-        lockPref("network.IDN_show_punycode", true);
-        lockPref("browser.tabs.tabmanager.enabled", false);
-        lockPref("dom.webnotifications.enabled", false);
-        lockPref("dom.webnotifications.serviceworker.enabled", false);
-      '';
     };
   };
 }
