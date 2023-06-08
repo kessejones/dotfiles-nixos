16fe2d5bf3afacb7367d048120cf2166461d18ac
Kesse Jones <kjonesfc@outlook.com>
2023-06-08 19:39:13 -0300

    feat: added transitions to picom

--- home-manager/modules/picom.nix
+++ home-manager/modules/picom.nix
@@ -6,7 +6,25 @@
     shadow = false;
     shadowOpacity = 0.3;
 
-    fade = false;
-    fadeDelta = 4;
+    fade = true;
+    fadeDelta = 1;
+    fadeSteps = [
+      0.01
+      0.01
+    ];
+
+    settings = {
+      detect-transient = true;
+      detect-client-loader = false;
+
+      no-fading-openclose = true;
+
+      transition-length = 300;
+      transition-pow-x = 0.1;
+      transition-pow-y = 0.1;
+      transition-pow-w = 0.1;
+      transition-pow-h = 0.1;
+      size-transition = true;
+    };
   };
 }
