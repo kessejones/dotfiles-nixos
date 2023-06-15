4f8712d562ec248403dce06c110387b7ce69e880
Kesse Jones <kjonesfc@outlook.com>
2023-06-14 23:31:40 -0300

    feat: added ssh prompt

--- home-manager/modules/fish/functions.nix
+++ home-manager/modules/fish/functions.nix
@@ -25,6 +25,11 @@ in {
           end
       end
 
+      if test -n "$SSH_TTY" || test -n "$SSH_CLIENT"
+          set -l machine (hostname)
+          _power_prompt --text="[$machine]" --background=$fish_color_lavender --foreground=$fish_color_base --reset=$reset_color --bold
+      end
+
       if test -n "$fish_private_mode"
           _power_prompt --text=' 󰗹 ' --background="292c3c" --foreground="cdd6f4" --reset=$reset_color --bold
       end
