c0c7529bc3be9ed751430448ad92b01565e330dd
Kesse Jones <kjonesfc@outlook.com>
2023-08-16 12:11:28 -0300

    feat(dunst): enabled dunst

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -19,4 +19,27 @@
   programs.go.enable = true;
   programs.zoxide.enable = true;
   services.udiskie.enable = true;
+
+  home.sessionPath = ["$HOME/.local/bin"];
+
+  services.dunst = {
+    enable = true;
+    settings = {
+      global = {
+        width = 300;
+        height = 300;
+        offset = "30x50";
+        origin = "top-right";
+        transparency = 10;
+        frame_color = "#eceff1";
+        font = "Droid Sans 9";
+      };
+
+      urgency_normal = {
+        background = "#37474f";
+        foreground = "#eceff1";
+        timeout = 10;
+      };
+    };
+  };
 }
