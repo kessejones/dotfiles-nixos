7e9f436ad59ebd1b4d162f661eb9cc9e14f0e1dd
Kesse Jones <kjonesfc@outlook.com>
2023-11-20 10:56:16 -0300

    fix: fixes udiskie require target.tray

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -43,4 +43,11 @@
       };
     };
   };
+
+  systemd.user.targets.tray = {
+    Unit = {
+      Description = "Home Manager System Tray";
+      Requires = ["graphical-session-pre.target"];
+    };
+  };
 }
