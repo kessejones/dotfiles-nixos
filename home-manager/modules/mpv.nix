654c150094ec92cc45d123d3a6fc6dcca72fb334
Kesse Jones <kjonesfc@outlook.com>
2024-03-28 22:47:33 -0300

    feat: added keybindings for mpv

--- home-manager/modules/mpv.nix
+++ home-manager/modules/mpv.nix
@@ -6,5 +6,13 @@
       slang = "por,jpn,eng";
       osd-on-seek = "no";
     };
+    bindings = {
+      WHEEL_UP = "seek 5";
+      WHEEL_DOWN = "seek -5";
+      "Shift+WHEEL_UP" = "seek 10";
+      "Shift+WHEEL_DOWN" = "seek -10";
+      "Ctrl+WHEEL_UP" = "add video-zoom 0.1";
+      "Ctrl+WHEEL_DOWN" = "add video-zoom -0.1";
+    };
   };
 }
