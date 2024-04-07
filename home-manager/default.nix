a59d9d5b2c3a496d3b65e16a8d18f7e0221c4f39
Kesse Jones <kjonesfc@outlook.com>
2024-04-07 10:30:14 -0300

    chore: remove dunst

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -23,27 +23,6 @@
 
   home.sessionPath = ["$HOME/.local/bin"];
 
-  services.dunst = {
-    enable = true;
-    settings = {
-      global = {
-        width = 300;
-        height = 300;
-        offset = "30x50";
-        origin = "top-right";
-        transparency = 10;
-        frame_color = "#eceff1";
-        font = "Droid Sans 9";
-      };
-
-      urgency_normal = {
-        background = "#37474f";
-        foreground = "#eceff1";
-        timeout = 10;
-      };
-    };
-  };
-
   systemd.user.targets.tray = {
     Unit = {
       Description = "Home Manager System Tray";
