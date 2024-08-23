59d0608640c602dbf53d95ddd55b26576e68a77b
Kesse Jones <kjonesfc@outlook.com>
2024-08-23 14:05:19 -0300

    chore: disable docker live restore for swarm

--- hosts/_common/modules/virtualisation.nix
+++ hosts/_common/modules/virtualisation.nix
@@ -1,9 +1,8 @@
-{pkgs, ...}: {
+{...}: {
   virtualisation = {
-    podman = {
-      enable = false;
-      defaultNetwork.settings.dns_name.enabled = true;
+    docker = {
+      enable = true;
+      liveRestore = false;
     };
-    docker.enable = true;
   };
 }
