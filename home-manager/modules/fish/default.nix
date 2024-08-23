d3db18d668a5273aead89cd6caeeb57355ebb027
Kesse Jones <kjonesfc@outlook.com>
2024-08-23 14:08:10 -0300

    chore: force fish cursor for wezterm

--- home-manager/modules/fish/default.nix
+++ home-manager/modules/fish/default.nix
@@ -13,6 +13,7 @@
       set fish_cursor_insert line
       set fish_cursor_replace underscore
       set fish_cursor_replace_one underscore
+      set fish_vi_force_cursor 1
 
       set fish_prompt_separator 0
 
