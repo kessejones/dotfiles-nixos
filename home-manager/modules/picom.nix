f5a59544b1fbd74500a09aa7f098e4d2e831ade7
Kesse Jones <kjonesfc@outlook.com>
2024-08-24 22:12:14 -0300

    chore: update picom config

--- home-manager/modules/picom.nix
+++ home-manager/modules/picom.nix
@@ -1,23 +1,42 @@
 {pkgs, ...}: {
   xdg.configFile."picom/picom.conf".text = ''
-    backend = "egl";
-
-    animations = ({
-    	triggers = ["geometry"];
-    	preset = "geometry-change";
-    	duration = 0.2;
-    });
-
-    shadow = true;
-    shadow-exclude = [];
-    shadow-radious = 12;
-    shadow-offset-x = -12;
-    shadow-offset-y = -12;
-    shadow-opacity = 0.3;
+    rules = (
+      {
+        match = "!fullscreen && (window_type != 'dock' && window_type != 'tooltip' && window_type != 'popup_menu' && window_type != 'utility')";
+        animations = ({
+          triggers = ["geometry"];
+          preset = "geometry-change";
+          duration = 0.2;
+        });
+      },
+    );
   '';
 
   services.picom = {
     enable = true;
     package = pkgs.picom-git;
+
+    backend = "glx";
+    shadow = false;
+    fade = false;
+    vSync = false;
+
+    opacityRules = [
+      "100:_NET_WM_STATE@:32a = '_NET_WM_STATE_FULLSCREEN'"
+      "0:_NET_WM_STATE@:32a *= '_NET_WM_STATE_HIDDEN'"
+      "0:_NET_WM_STATE@[0]:32a *= '_NET_WM_STATE_HIDDEN'"
+      "0:_NET_WM_STATE@[1]:32a *= '_NET_WM_STATE_HIDDEN'"
+      "0:_NET_WM_STATE@[2]:32a *= '_NET_WM_STATE_HIDDEN'"
+      "0:_NET_WM_STATE@[3]:32a *= '_NET_WM_STATE_HIDDEN'"
+      "0:_NET_WM_STATE@[4]:32a *= '_NET_WM_STATE_HIDDEN'"
+      "98:class_g = 'xst-256color'"
+      "90:class_g = 'xst-scratch'"
+    ];
+
+    settings = {
+      unredir-if-possible = true;
+      glx-no-stencil = true;
+      xrender-sync-fence = true;
+    };
   };
 }
