384cbac45a6c40c130a91da08c65a02a3590ef27
Kesse Jones <kjonesfc@outlook.com>
2025-04-22 23:49:42 -0300

    chore: added default apps for some mime types

--- home-manager/modules/xdg.nix
+++ home-manager/modules/xdg.nix
@@ -5,6 +5,16 @@
   xdg.mimeApps.enable = true;
   xdg.mimeApps.defaultApplications = {
     "inode/directory" = "nemo.desktop";
+
+    "image/png" = "pix.desktop";
+    "image/jpg" = "pix.desktop";
+    "image/jpeg" = "pix.desktop";
+
+    "application/pdf" = "zathura.desktop";
+
+    "application/x-torrent" = "deluge.desktop";
+    "x-scheme-handler/magnet" = "deluge.desktop";
+
     "text/html" = "librewolf.desktop";
     "x-scheme-handler/http" = "librewolf.desktop";
     "x-scheme-handler/https" = "librewolf.desktop";
