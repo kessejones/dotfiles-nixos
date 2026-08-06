4ad8090fe559533fa6bcdf5e0b8e6ef3eef03e9a
Kesse Jones <kjonesfc@outlook.com>
2026-08-05 21:37:55 -0300

    chore: update flakes

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -14,12 +14,12 @@
   };
 
   floorp-bin-unwrapped = let
-    version = "12.14.0";
+    version = "12.16.4";
   in
     prev.floorp-bin-unwrapped.overrideAttrs (old: {
       src = prev.fetchurl {
         url = "https://github.com/Floorp-Projects/Floorp/releases/download/v${version}/floorp-linux-x86_64.tar.xz";
-        hash = "sha256-D8O38ZLJrh3vOhYMaKLe5CyKAnfBtI3u3gvR8txp4xc=";
+        hash = "sha256-UZL88awADg9eVtCMsIjeeZZxlYVz0v5dQrg1LxWZvfc=";
       };
     });
 
